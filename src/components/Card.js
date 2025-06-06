28d7d57dc4e420dafcb4d25ae00b15a7c70d0335
Tiago Costa <tiagocostacj@gmail.com>
2025-06-05 21:45:06 -0300

    atualização
    ok

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,20 @@
 import styles from "./Card.module.css";
+import Link from "next/link";
 
 export default function Card() {
   return (
-    <div className={styles.card}>
-      <h2>Canções TFM</h2>
+    <div className={styles.container}>
+      <Link href="/cancoes" className={styles.card}>
+        <h2>Canções TFM</h2>
+      </Link>
+
+      <Link href="/oracoes" className={styles.card}>
+        <h2>Orações</h2>
+      </Link>
+
+      <Link href="/hinos" className={styles.card}>
+        <h2>Hinos</h2>
+      </Link>
     </div>
   );
 }
